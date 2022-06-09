74190739019cd60220276fdd34959f43ed844a25
Sebastian Delgado <sede2.pcm@procivitas.se>
2022-06-09 20:51:37 +0200

    add navbar and hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react"
-import {
-  ChakraProvider,
-  theme,
-} from "@chakra-ui/react"
+import {ChakraProvider,theme,} from "@chakra-ui/react"
+import {MainNav} from './Nav/MainNav'
+import {MainBanner} from './Banners/MainBanner'
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    
+    <MainNav />
+    <MainBanner />
   </ChakraProvider>
 )
